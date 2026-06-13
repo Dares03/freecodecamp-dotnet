5a026e3b5b70a3589575ae031662948ff66498ba
Dares03 <josshuadavid5g@gmail.com>
2026-06-13 07:01:11 -0500

    feat(BooleanExpressions): Probando operador condicional

--- BooleanExpressions/Program.cs
+++ BooleanExpressions/Program.cs
@@ -2,6 +2,10 @@
 //string value2 = " A";
 //Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
+
+int saleAmount = 1001;
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");
